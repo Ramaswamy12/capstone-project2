68a13b030ffdcc3696def05b16d228e4f284766b
Ramaswamy12 <142513015+Ramaswamy12@users.noreply.github.com>
2023-11-10 15:34:01 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,13 @@ docker login -u "$DOCKERHUB_USERNAME" -p "$DOCKERHUB_PASSWORD"
 
 IMAGE_NAME="myreactimg"
 IMAGE_TAG="latest"
-if [[ CURRENT_BRANCH=$(git rev-parse --abbrev-ref master) ]]; then
+if [[ CURRENT_BRANCH=$(git rev-parse --abbrev-ref dev) ]]; then
   echo "Current branch: $CURRENT_BRANCH" 
   ./build.sh
   docker tag "$IMAGE_NAME:$IMAGE_TAG" "$DOCKERHUB_USERNAME/$DEV_REPO:$IMAGE_TAG"
   docker push "$DOCKERHUB_USERNAME/$DEV_REPO:$IMAGE_TAG"
   echo "Docker image pushed to dev repo on DockerHub."
-elif [[ CURRENT_BRANCH=$(git rev-parse --abbrev-ref dev) ]]; then
+elif [[ CURRENT_BRANCH=$(git rev-parse --abbrev-ref master) ]]; then
   echo "Current branch: $CURRENT_BRANCH" 
   ./build.sh
   docker tag "$IMAGE_NAME:$IMAGE_TAG" "$DOCKERHUB_USERNAME/$PROD_REPO:$IMAGE_TAG"
@@ -22,5 +22,3 @@ elif [[ CURRENT_BRANCH=$(git rev-parse --abbrev-ref dev) ]]; then
 else
   echo "Code error: Unsupported branch for deployment."
 fi
-
-
