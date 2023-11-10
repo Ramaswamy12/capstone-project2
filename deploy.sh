b70abc1d45701e5069fc766ff9fee26da7c887ac
Ramaswamy12 <142513015+Ramaswamy12@users.noreply.github.com>
2023-11-10 15:59:22 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,22 +5,24 @@ PROD_REPO="prod"
 DOCKERHUB_PASSWORD="dckr_pat_W8WCZwTlaOGcNsEniO_3nRGe0Dc"
 docker login -u "$DOCKERHUB_USERNAME" -p "$DOCKERHUB_PASSWORD"
 
-IMAGE_NAME="myreactimg"
-IMAGE_TAG="latest"
-if [[ CURRENT_BRANCH=$(git rev-parse --abbrev-ref dev) ]]; then
-  echo "Current branch: $CURRENT_BRANCH" 
-  ./build.sh
-  docker tag "$IMAGE_NAME:$IMAGE_TAG" "$DOCKERHUB_USERNAME/$DEV_REPO:$IMAGE_TAG"
-  docker push "$DOCKERHUB_USERNAME/$DEV_REPO:$IMAGE_TAG"
-  echo "Docker image pushed to dev repo on DockerHub."
-elif [[ CURRENT_BRANCH=$(git rev-parse --abbrev-ref master) ]]; then
-  echo "Current branch: $CURRENT_BRANCH" 
-  ./build.sh
-  docker tag "$IMAGE_NAME:$IMAGE_TAG" "$DOCKERHUB_USERNAME/$PROD_REPO:$IMAGE_TAG"
-  docker push "$DOCKERHUB_USERNAME/$PROD_REPO:$IMAGE_TAG"
-  echo "Docker image pushed to prod repo on DockerHub."
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
+    # Build your project
+    ./build.sh
+
+    # Log in to Docker Hub (replace with your actual Docker Hub credentials)
+    
+  
+    # Tag the image
+    docker tag myreactimg ramaswamy12/dev
+
+    # Push the image to the Dev Docker Hub repository
+    docker push ramaswamy12/dev
+
+elif [[ $GIT_BRANCH == "origin/master" ]]; then
+    # Build your project
+    ./build.sh
+     docker tag myreactimg ramaswamy12/prod
+      docker push ramaswamy12/prod
 else
-  echo "Code error: Unsupported branch for deployment."
+  echo "code not deployed"
 fi
-
-
