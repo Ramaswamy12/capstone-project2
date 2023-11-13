3d5f2629d9d9d3a08250b97fdd9c1d328d9d82dd
Ramaswamy12 <142513015+Ramaswamy12@users.noreply.github.com>
2023-11-13 15:34:52 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
-DOCKERHUB_USERNAME="ramaswamy12"
-DEV_REPO="dev"
-PROD_REPO="prod"
-DOCKERHUB_PASSWORD="dckr_pat_W8WCZwTlaOGcNsEniO_3nRGe0Dc"
-docker login -u "$DOCKERHUB_USERNAME" -p "$DOCKERHUB_PASSWORD"
-
+docker login -u "ramaswamy12" -p "dckr_pat_W8WCZwTlaOGcNsEniO_3nRGe0Dc"
+# Check the Git branch
 if [[ $GIT_BRANCH == "origin/dev" ]]; then
     # Build your project
     ./build.sh
-
     # Log in to Docker Hub (replace with your actual Docker Hub credentials)
-    
-  
-    # Tag the image
+   :wq
+   # Tag the image
     docker tag myreactimg ramaswamy12/dev
 
     # Push the image to the Dev Docker Hub repository
